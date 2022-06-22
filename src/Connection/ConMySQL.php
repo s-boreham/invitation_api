5f574b0c1f1ef019a56e5049fa6a9c0c91de358a
s-boreham <106034887+s-boreham@users.noreply.github.com>
2022-06-22 09:29:40 +0200

    Add files via upload

--- src/Connection/ConMySQL.php
+++ src/Connection/ConMySQL.php
@@ -8,7 +8,7 @@ use PDO;
 class ConMySQL {
 
     public function ConToMySQL1() {
-        $pdo = new PDO('mysql:host=localhost;dbname=invitations_api_db;charset=utf8','scholar','Davie504');
+        $pdo = new PDO('mysql:host=localhost;dbname=invitations_api_db;charset=utf8','admin','admin');
         $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
         return $pdo;
     }
